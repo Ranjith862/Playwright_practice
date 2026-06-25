d27e05d430332e968358b67694a51a3b63dd9f68
Ranjith862 <ranjit862002@gmail.com>
2026-06-25 13:16:40 +0530

    update

--- tests/automation_exercise.spec.ts
+++ tests/automation_exercise.spec.ts
@@ -15,7 +15,10 @@ test("Practice of getBy methods", async ({ page }) => {
     let passwordInput: Locator = page.getByPlaceholder("Email Address").nth(1); //Get loctor by xpath
     await passwordInput.fill("admin123@mailinator.com"); //Get loctor by xpath and fill the value
     console.log("User successfully entered password");
-    await page.getByRole("button", { name: "Signup" }).click(); //Get loctor by role and click the button    
+    
+    let signUpBtn: Locator = page.getByRole("button", { name: "Signup" });
+    signUpBtn.click(); //Get loctor by role and click the button    
     console.log("Sign Up successfully");
+
     await page.close(); 
 });
